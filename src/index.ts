a02c3dbcd4e5489fa60396533d8b7ece065a8fcf
byteful <info@byteful.me>
2025-05-03 21:46:44 -0400

    Initial setup

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import 'dotenv/config';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on(Events.ClientReady, readyClient => {
+  console.log(`Logged in as ${readyClient.user.tag}!`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
